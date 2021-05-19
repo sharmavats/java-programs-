52db42e6ca34596db7dd1b0be3803106f79d8579
sharmavats <kamal251199@gmail.com>
2021-05-19 23:44:20 +0530

    17th program

---
+++ _17stringdemo.java
@@ -0,0 +1,11 @@
+public class _17stringdemo {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        char arr[]={'J','A','V','A'};
+        String str2 = new String("String demo");
+        String str3 = new String(arr);
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("String 3: " + str3);
+    }
+}
